e724fb4e8f54d0bb44e42bb3d56e56064abcd1d9
Lars Kusch <lars@lars-kusch.de>
2025-12-28 07:52:27 +0100

    Fixes

---
+++ eslint.config.mjs
@@ -0,0 +1,46 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+import globals from "globals";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default defineConfig([globalIgnores(["tests/**/*.ts"]), {
+    extends: compat.extends("eslint:recommended"),
+
+    languageOptions: {
+        globals: {
+            ...globals.browser,
+        },
+
+        ecmaVersion: "latest",
+        sourceType: "module",
+    },
+
+    rules: {
+        "no-unused-vars": ["warn", {
+            argsIgnorePattern: "^_",
+        }],
+
+        "no-constant-binary-expression": "off",
+    },
+}, {
+    files: ["tests/**/*.ts"],
+    plugins: {},
+
+    languageOptions: {
+        globals: {
+            ...globals.node,
+        },
+    },
+
+    rules: {},
+}]);
